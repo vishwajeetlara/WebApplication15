33e1f41abfd537f5b6f5206d353d536cd21bd9e8
Vishwajeet Sahu <Vishwajeet_Sahu@Dellteam.com>
2021-03-16 00:48:40 +0530

    Add project files.

---
+++ WebApplication15/Controllers/MoviesController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication15.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies
+        public ActionResult RandomMovi()
+        {
+            return View();
+        }
+
+
+        public ActionResult NewMovie()
+        {
+            return View();
+        }
+        public ActionResult ReleaseMoveByDate()
+        {
+            return View();
+        }
+    }
+}
---
+++ WebApplication15/Controllers/ProdcutController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication15.Controllers
+{
+    // control action method return type 
+    public class ProdcutController : Controller
+    {
+     
+        public ActionResult RandomMovi()
+        {
+
+            return RedirectToAction("User");
+        }
+
+        public ActionResult User()
+        {
+
+            return View();
+        }
+    }
+}
